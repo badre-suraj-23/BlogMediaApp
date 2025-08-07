7fa3757981e737bf905cc43065fdcf9588e9aaa3
suraj badre <badresuraj@gmail.com>
2025-08-08 02:26:29 +0530

    Corrected Procfile for Render deployment

--- base/settings.py
+++ base/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['blogmediaapp.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['blogmedia.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
